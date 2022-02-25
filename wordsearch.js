76be27f89479604f27c84a372660d37c6a1e01c0
Rachel PR <rplourder@gmail.com>
2022-02-24 22:21:34 -0400

    Add a comment about this file

--- wordsearch.js
+++ wordsearch.js
@@ -1,32 +1,21 @@
+//A function that searches through a letters array to find a given word
 const wordSearch = (letters, word) => {
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (let i = 0; i < letters.length; i++) {
-        let verticalJoin = letters.map((ls) => ls[i]).join('')
+        let verticalJoin = letters.map((ls) => ls[i]).join('');
         let reversedV = verticalJoin.split("").reverse().join("");
         if (verticalJoin.includes(word) || reversedV.includes(word)) {
             return true;
-        }
-    }
+        };
+    };
     for (l of horizontalJoin) {
-        let reversedL = l.split("").reverse().join("")
+        let reversedL = l.split("").reverse().join("");
         if (l.includes(word) || reversedL.includes(word)) {
             return true;
         }
-    }
+    };
     return false;
-
 }
-console.log((wordSearch([
-    ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-    ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-    ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-    ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-    ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
-    ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-    ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
-    ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
-    ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-], "WEST")));
 
 module.exports = wordSearch
 
